e5e5a5337f3f27dce3038b8701fb125d36190d45
ehghksvjscl <ehghkswodnjs@naver.com>
2022-08-16 00:23:02 +0900

    fix: test_command file

--- recipe-app-api/app/app/settings.py
+++ recipe-app-api/app/app/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -88,7 +89,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    },
 }
 
 
--- recipe-app-api/app/core/management/commands/wait_for_db.py
+++ recipe-app-api/app/core/management/commands/wait_for_db.py
@@ -1,12 +1,30 @@
 """
 Django command to wait for the database to be available.
 """
+import time
+
 from typing import Any, Optional
 from django.core.management.base import BaseCommand
+from django.db.utils import OperationalError
+
+from psycopg2 import OperationalError as Psycopg2Error
 
 
 class Command(BaseCommand):
     """Django command to wait for database"""
 
     def handle(self, *args: Any, **options: Any) -> Optional[str]:
-        pass
+        """Entrypoint for command"""
+        self.stdout.write('데이터베이스 기다리는중...')
+        db_up = False
+
+        while db_up is False:
+            try:
+                self.check(databases=['default'])
+                db_up = True
+            except (Psycopg2Error, OperationalError):
+                self.stdout.write('데이터베이스 없음 1초 대기')
+                time.sleep(1)
+
+        self.stdout.write(self.style.SUCCESS("데이터베이스 연결됨!"))
+                
