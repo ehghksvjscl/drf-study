1d4604888fd7c1fc62e6fc104a1863b487e57d83
ehghksvjscl <ehghkswodnjs@naver.com>
2022-08-18 16:51:52 +0900

    feat: superuser 생성 함수

--- recipe-app-api/app/core/models.py
+++ recipe-app-api/app/core/models.py
@@ -18,15 +18,27 @@ from django.contrib.auth.models import (
 class UserManaager(BaseUserManager):
     """Manager for user"""
 
-    def create_user(self, email, password=None, **extra_field):
+    def create_user(self, email, password=None, **extra_fields):
         """Create, save and return a new user"""
 
-        user = self.model(email, **extra_field)
+        if not email:
+            raise ValueError('User must have an email address')
+
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
+        # save 하는법(특이함..)
         user.save(using=self._db)
 
         return user
 
+    def create_superuser(self, email, password):
+        """ Create Super user"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
 
 class User(AbstractBaseUser, PermissionsMixin):
     """User in the system"""
@@ -35,6 +47,6 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
-    objects: UserManaager
+    objects = UserManaager()
 
     USERNAME_FIELD = "email"
--- recipe-app-api/app/core/tests/test_models.py
+++ recipe-app-api/app/core/tests/test_models.py
@@ -2,6 +2,7 @@
 Tests for models.
 """
 
+from random import sample
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
@@ -20,3 +21,34 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test email is normalized for new users"""
+
+        sample_emails = [
+            ['test1@EXAMPLE.com', 'test1@example.com'],
+            ['Test2@Example.com', 'Test2@example.com'],
+            ['TEST3@EXAMPLE.com', 'TEST3@example.com'],
+            ['test4@example.COM', 'test4@example.com'],
+        ]
+
+        for email, expect in sample_emails:
+            user = get_user_model().objects.create_user(email, 'sample123')
+            self.assertEqual(user.email, expect)
+
+    def test_new_user_without_email_raiises_error(self):
+        """Test 유저를 생성할때 에러"""
+
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'test123')
+
+    def test_create_superuser(self):
+        """Test 슈퍼유저 생성"""
+        
+        user = get_user_model().objects.create_superuser(
+            'test@example.com',
+            'test1234'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
