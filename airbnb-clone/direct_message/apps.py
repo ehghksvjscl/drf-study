3551d4897dd73048212e3a7c823ca74aca957899
ehghksvjscl <ehghkswodnjs@naver.com>
2022-09-26 14:56:07 +0900

    feat: direct_message model

--- airbnb-clone/direct_message/apps.py
+++ airbnb-clone/direct_message/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class DirectMessageConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "direct_message"
+    verbose_name = "Direct Message"
--- airbnb-clone/direct_message/models.py
+++ airbnb-clone/direct_message/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from common.models import BaseTimeTamplate
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class ChattingRoom(BaseTimeTamplate):
+    """메시지 룸"""
+
+    user = models.ManyToManyField(get_user_model())
+
+    def __str__(self) -> str:
+        return "Chatting Room"
+
+
+class Message(BaseTimeTamplate):
+    """메시지 모델링"""
+
+    text = models.TextField()
+    user = models.ForeignKey(
+        get_user_model(), on_delete=models.SET_NULL, null=True, blank=True
+    )
+    room = models.ForeignKey("direct_message.ChattingRoom", on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f"{self.user} says: {self.text}"
