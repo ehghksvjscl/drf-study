131f2d7c3a7e702819005f3d9d334b6509b6fcd8
ehghksvjscl <ehghkswodnjs@naver.com>
2022-10-14 14:39:01 +0900

    feat: Amenity and Detail API

--- airbnb-clone/rooms/views.py
+++ airbnb-clone/rooms/views.py
@@ -1,6 +1,47 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from rooms.models import Amenity
+from rooms.serializers import AmenitySerializer
 
 
-def sayHello(request):
-    return HttpResponse("hello!")
+class Amenities(APIView):
+    def get(self, request):
+        amenities = Amenity.objects.all()
+        serializer = AmenitySerializer(amenities, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = AmenitySerializer(data=request.data)
+        if serializer.is_valid():
+            amenity = serializer.save()
+            return Response(AmenitySerializer(amenity).data)
+        else:
+            return Response(serializer.errors)
+
+class AmenityDetail(APIView):
+
+    def get_object(self, pk):
+        return get_object_or_404(Amenity, pk=pk)
+
+    def get(self, request, pk: int):
+        amenity = self.get_object(pk)
+        serializer = AmenitySerializer(amenity)
+        return Response(serializer.data)
+
+    def put(self, request, pk: int):
+        amenity = self.get_object(pk)
+        serializer = AmenitySerializer(amenity, data=request.data, partial=True)
+        if serializer.is_valid():
+            amenity = serializer.save()
+            return Response(AmenitySerializer(amenity).data)
+        else:
+            return Response(serializer.errors)
+
+    def delete(self, request, pk: int):
+        amenity = self.get_object(pk)
+        amenity.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
