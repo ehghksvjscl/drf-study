c629c5ba9020a9fd7c2cd72841b9e50d42ef11c1
ehghksvjscl <ehghkswodnjs@naver.com>
2022-10-26 19:03:39 +0900

    feat: user change password

--- airbnb-clone/users/views.py
+++ airbnb-clone/users/views.py
@@ -1,10 +1,13 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.exceptions import ParseError
+from rest_framework import status
+
+from django.shortcuts import get_object_or_404
 
 from .serializers import PrivateUserSerializer
+from users.models import User
 
 
 class Me(APIView):
@@ -43,3 +46,32 @@ class Users(APIView):
             return Response(serializer.data)
         else:
             return Response(serializer.errors)
+
+
+class PublicUser(APIView):
+    def get_user(self, username):
+        return get_object_or_404(User, username=username)
+
+    def get(self, request, username):
+        user = self.get_user(username)
+        return Response(PrivateUserSerializer(user).data)
+
+
+class ChangePassword(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def put(self, request):
+        user = request.user
+        old_password = request.data.get("old_password")
+        new_password = request.data.get("new_password")
+
+        if not old_password or not new_password:
+            raise ParseError
+
+        if user.check_password(old_password):
+            user.set_password(new_password)
+            user.save()
+            return Response(status=status.HTTP_200_OK)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
