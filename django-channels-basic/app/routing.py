749c6a7cb6b13d6f33d50750ffc0064bb4be01c2
ehghksvjscl <ehghkswodnjs@naver.com>
2023-01-22 00:18:22 +0900

    liveblog Consumer 구현

--- django-channels-basic/app/routing.py
+++ django-channels-basic/app/routing.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from app.consumers import EchoConsumer
+from app.consumers import EchoConsumer, LiveBlogConsumer
 
 websocket_urlpatterns = [
+    path('ws/liveblog/', LiveBlogConsumer.as_asgi()),
     path('ws/echo/', EchoConsumer.as_asgi()),
 ]
