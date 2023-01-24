97e3f62ab4bfe20f46ac664fc6ec5ac9b118188a
ehghksvjscl <ehghkswodnjs@naver.com>
2023-01-24 12:33:57 +0900

    feat: chat consumer 구현

--- django-channels-basic/chat/consumers.py
+++ django-channels-basic/chat/consumers.py
@@ -1,6 +1,29 @@
-import json
-
+from channels.layers import get_channel_layer
 from channels.generic.websocket import JsonWebsocketConsumer
 
+from asgiref.sync import async_to_sync
+
 class ChatConsumer(JsonWebsocketConsumer):
-    pass
+    SQUARER_GROUP_NAME = "square"
+    groups = [SQUARER_GROUP_NAME]
+
+    def recevie_json(self, content, **kwargs):
+        _type = [content['type']]
+
+        if _type == "chat.message":
+            message = content['message']
+            async_to_sync(self.channel_layer.group_send)(
+                self.SQUARER_GROUP_NAME,
+                {
+                    "type": "chat.message",
+                    "message": message
+                }
+            )
+        else:
+            print(f"Invalid message type: {_type}")
+
+    def chat_message(self, message_dict):
+        self.send_json({
+            "type": "chat.message",
+            "message": message_dict['message']
+        })
