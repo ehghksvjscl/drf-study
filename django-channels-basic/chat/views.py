e22a347c73aa13e1d5e1711d70e1bfcbb5bb4984
ehghksvjscl <ehghkswodnjs@naver.com>
2023-01-25 01:50:54 +0900

    feat: 체팅방 삭제하기

--- django-channels-basic/chat/views.py
+++ django-channels-basic/chat/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 from chat.forms import RoomForm
 from chat.models import Room
@@ -33,4 +34,21 @@ def room_new(request):
 
     return render(request, 'chat/room_form.html', {
         "form": form,
+    })
+
+@login_required
+def room_delete(request, room_pk):
+    room = get_object_or_404(Room, pk=room_pk)
+
+    if request.user != room.owner:
+            messages.error("방을 삭제할 권한이 없습니다.")
+            return redirect('chat:index')
+
+    if request.method == "POST":
+        room.delete()
+        messages.success(request, "방을 삭제했습니다.")
+        return redirect('chat:index')
+
+    return render(request, 'chat/room_confirm_delete.html', {
+        "room": room,
     })
